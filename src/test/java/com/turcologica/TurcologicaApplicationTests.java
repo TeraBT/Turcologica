98c0c7ad4ec2f3e90443f8ba94b329d631b3223f
TeraBT <chris.baumgartner115@gmail.com>
2023-10-31 02:14:38 +0100

    init

---
+++ src/test/java/com/turcologica/TurcologicaApplicationTests.java
@@ -0,0 +1,12 @@
+package com.turcologica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TurcologicaApplicationTests {
+	@Test
+	void contextLoads() {
+	}
+
+}
