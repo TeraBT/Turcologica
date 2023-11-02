be5ba28df6cd5ef7f9de443dea73d73ec3b69f3f
TeraBT <chris.baumgartner115@gmail.com>
2023-11-02 12:59:12 +0100

    fixed dependencies

--- src/main/java/com/turcologica/TurcologicaApplication.java
+++ src/main/java/com/turcologica/TurcologicaApplication.java
@@ -7,15 +7,9 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@RestController
 public class TurcologicaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TurcologicaApplication.class, args);
-	}
-
-	@GetMapping("/hello")
-	public String sayHello(@RequestParam(value = "name", defaultValue = "World") String name) {
-		return String.format("Hello %s!", name);
 	}
 }
